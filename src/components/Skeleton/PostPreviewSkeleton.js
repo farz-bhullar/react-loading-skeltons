7b4e84868627b6ed62c37dc7b54bb5cffeca81ba
Farz Bhullar <farz.bhullar@gmail.com>
2021-04-11 00:27:03 +0530

    formatted skeleton for post preview

--- src/components/Skeleton/PostPreviewSkeleton.js
+++ src/components/Skeleton/PostPreviewSkeleton.js
@@ -6,8 +6,8 @@ function PostPreviewSkeleton() {
   return (
     <div className="skeletonWrapper">
       <Skeleton width="50%" height="25px" />
-      <Skeleton width="100%" height="300px" marginTop="50px" />
-      {Array(8)
+      <Skeleton width="100%" height="200px" marginTop="25px" />
+      {Array(4)
         .fill()
         .map(() => (
           <Skeleton width="80%" height="20px" />
